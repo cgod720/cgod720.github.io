6ed4acac34b956f59fe86a53add65160f9d1b5df
Carlos Godoy <cgod720@gmail.com>
2019-01-11 01:19:53 -0500

    added sworn members function

--- westeros/app.js
+++ westeros/app.js
@@ -1,33 +1,74 @@
 $(() => {
-  // const $getHouse = (event) => {
-  //   const $house = $(event.currentTarget).attr('id');
-  // }
 
-  $('button').on('click', (event) => {
-    // console.log("here");
-    event.preventDefault();
-    const $id = $(event.currentTarget).attr('id');
+  const getswornMembers = (url) => {
     $.ajax(
       {
-        url: "https://anapioficeandfire.com/api/houses/" + $id,
-        type: "GET",
-        // data: {
-        //   "$limit" : $number
-        // }
+        url: url,
+        type: "GET"
+      }).then(
+        (data) => {
+        const $button = $('<button>').html(data.name).addClass("member")
+        $('#swornMembers').append($button)
+        console.log(data)
+        },
+        () => {
+          console.log("bad member data");
+        }
+      )
+  }
 
-    }).then(
-      (data) => {
-          console.log(data);
-          for(i = 0; i < 10; i++){
-            const $div = $('<div>').html(data.swornMembers[i])
-            $('#swornMembers').append($div)
-          }
+  const getLord = (url) => {
+      console.log(url)
+      $.ajax(
+        {
+          url: url,
+          type: "GET"
+      }).then(
+        (data) => {
+          console.log(data.name)
+          $('#lord').html("Current Lord: " + data.name);
+        },
+        () => {
+          $('#lord').html("");
+        }
+      )
+  }
+
+
+  $('.house').on('click', (event) => {
+
+    //Clears any members currently listed
+    $('#swornMembers').empty();
+
+    const $id = $(event.currentTarget).attr('id');
+
+    $.ajax(
+      {
+        url: "https://anapioficeandfire.com/api/houses/"+ $id,
+        type: "GET",
+
+      }).then(
+          (data) => {
+            console.log(data);
+            $('#of').html(data.name);
+            getLord(data.currentLord);
+            console.log(data.currentLord);
+            for(i = 0; i < data.swornMembers.length; i++){
+              getswornMembers(data.swornMembers[i]);
+            }
       },
-      () => {
-        console.log("bad data");
-      }
+        () => {
+          console.log("bad data");
+        }
     );
 
   })
+
+  // $('.member').on('click', () => {
+  //   const $div = $('<div>').html("hey")
+  //   $modal.append($div);
+  //
+  // })
+
 
 })
